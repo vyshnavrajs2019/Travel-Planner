a16724f11e97540314c86f19b69c5b659f52c1c6
Vyshnav <svyshnavraj@gmail.com>
2020-03-25 10:07:33 +0530

    Added save

--- data.py
+++ data.py
@@ -7,9 +7,23 @@ def load_data():
 		content = data.read()
 		return json.loads(content)
 
+# Save data
+def save_data():
+	file_name = "save.json"
+	with open(file_name, "w") as data:
+		data.write(json.dumps(places))
+
 # Get data
 places = load_data()
 
 for place in places:
 	if places[place]['TAG'].strip() == "":
-		print(place)
+		print(place, places[place]['ADDRESS'])
+		tag = input('TAG: ')
+		places[place]['TAG'] = tag
+	choice = input('Save: y/n')
+
+
+save_data()
+
+
