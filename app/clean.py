e1fb31548272fa1ba8ce201798bc2db997e1d89e
zameel123 <pamohamedzameel@gmail.com>
2020-04-02 14:05:03 +0530

    added weigths to the tags and route algoritm modifed

--- app/clean.py
+++ app/clean.py
@@ -5,17 +5,20 @@ content = {}
 with open('data/sample.json') as sample:
 	content = json.loads(sample.read())
 
-tags = set()
-for place in content:
-	tags.add(content[place]['TAG'])
+with open('data/tags.txt') as tags:
+	tags_cost = json.loads(tags.read())
 
-for tag in tags:
-	print(tag.upper())
-	cost = float(input("COST: "))
+# tags = set()
+# for place in content:
+# 	tags.add(content[place]['TAG'])
+
+for tag in tags_cost:
+	# print(tag.upper())
+	# cost = float(input("COST: "))
 	for place in content:
 		if content[place]['TAG'] == tag:
 			rating = float(content[place]['RATING'])
-			content[place]['COST'] = cost * (rating/5)
+			content[place]['COST'] = tags_cost[tag] * (rating/5)
 
 with open('sample2.json', 'w') as sample:
 	sample.write(json.dumps(content))
